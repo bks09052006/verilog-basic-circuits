7df2d985e6e8d447f5af725cf48eab6fc0b13d7c
bks09052006 <bks09052006@gmail.com>
2026-09-02 21:24:51 +0530

    Add files via upload

---
+++ full_subtractor.v
@@ -0,0 +1,10 @@
+module full_subtractor(
+  input wire a,
+  input wire b,
+  input wire bin,
+  output wire diff,
+  output wire bout
+);
+  assign diff = a ^ b ^ bin;
+  assign bout = (~a & b) | (~a & bin) | (b & bin);
+endmodule
