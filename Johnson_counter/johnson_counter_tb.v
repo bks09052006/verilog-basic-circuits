142e38ee29b44a1daab300fe608ba7f60ee71527
bks09052006 <bks09052006@gmail.com>
2026-09-12 07:17:41 -0400

    Add files via upload

---
+++ Johnson_counter/johnson_counter_tb.v
@@ -0,0 +1,15 @@
+module johnson_counter_tb;
+    reg clk, reset;
+    wire [3:0] q;
+
+    johnson_counter uut(.clk(clk), .reset(reset), .q(q));
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        #10 reset = 0;
+        #100 $finish;
+    end
+endmodule
