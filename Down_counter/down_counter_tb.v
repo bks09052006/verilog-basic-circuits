a7092fc480e86610f075926f66df4614d75e247b
bks09052006 <bks09052006@gmail.com>
2026-09-12 06:25:03 -0400

    Add files via upload

---
+++ Down_counter/down_counter_tb.v
@@ -0,0 +1,15 @@
+module down_counter_tb;
+    reg clk, reset;
+    wire [3:0] q;
+
+    down_counter uut(.clk(clk), .reset(reset), .q(q));
+
+    always #5 clk = ~clk;
+
+    initial begin
+        clk = 0;
+        reset = 1;
+        #10 reset = 0;
+        #200 $finish;
+    end
+endmodule
